d0ec9c4b6f9bfe11ee1aece76acd15d1524fbc7e
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-08-29 18:00:41 +0200

    :bug: Fix download_models script

--- download_models.sh
+++ download_models.sh
@@ -16,14 +16,14 @@ git clone https://github.com/ggerganov/llama.cpp.git
 cd llama.cpp && git checkout bcce96ba4dd95482824700c4ce2455fe8c49055a && cd ..
 
 # We install the python3 packages required to convert the llama2 model to gguf
-RUN pip install -r requirements.txt
+cd llama.cpp && pip install -r requirements.txt && cd ..
 
 # We convert the llama2 model from ggmlv3 to gguf
 python llama.cpp/convert-llama-ggmlv3-to-gguf.py --input $MODEL_DIR/llama-2-7b-chat.ggmlv3.q2_K.bin --output $MODEL_DIR/llama-2-7b-chat.gguf.q2_K.bin
 rm $MODEL_DIR/llama-2-7b-chat.ggmlv3.q2_K.bin
 
-
 # We convert the llama2 model from ggmlv3 to gguf
-python convert-llama-ggmlv3-to-gguf.py --input $MODEL_DIR/llama-7b-ggml-v3-q4_0.bin --output $MODEL_DIR/llama-7b.gguf.q4_0.bin
+python llama.cpp/convert-llama-ggmlv3-to-gguf.py --input $MODEL_DIR/llama-7b-ggml-v3-q4_0.bin --output $MODEL_DIR/llama-7b.gguf.q4_0.bin
 rm $MODEL_DIR/llama-7b-ggml-v3-q4_0.bin
 
+rm -rf llama.cpp
